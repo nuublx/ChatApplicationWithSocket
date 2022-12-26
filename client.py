495967566f4121e48aeb2ac9ea4647fc5f030ee0
nuublx <zeyaddiaa21@gmail.com>
2022-12-26 17:14:05 +0200

    Added RSA Encryption for messages between client and server

--- client.py
+++ client.py
@@ -1,32 +1,37 @@
 import socket
 import threading
+import rsa
 
 HOST = '127.0.0.1'
 PORT = 1234
 
 
-def receive_messages(client, username):
+def receive_messages(client, username, private_key):
     while True:
-        message = client.recv(2048).decode('utf-8')
+        message = client.recv(2048)
+        message = rsa.decrypt(message, private_key)
         if message:
+            message=message.decode()
             user, content = message.split('~')
             if user != username:
                 print(f'\n{message}')
 
 
-def chat(client):
+def chat(client, server_key):
     while True:
         message = input()
         if message:
-            client.sendall(message.encode())
+            message = rsa.encrypt(message.encode(), server_key)
+            client.sendall(message)
 
 
-def send_username(client):
+def send_username(client, public_key):
     username = input("Enter username: ")
     while username == "":
         print("Username cannot be empty, try again!")
         username = input("Enter username: ")
     client.sendall(username.encode())
+    client.sendall(public_key.save_pkcs1("PEM"))
     return username
 
 
@@ -42,13 +47,17 @@ def connect_to_server():
 
 
 def main():
+    public_key, private_key = rsa.newkeys(1024)
     client = connect_to_server()
     if client is not None:
-        username = send_username(client)
+        username = send_username(client, public_key)
 
-        threading.Thread(target=receive_messages, args=(client, username)).start()
+        server_key = client.recv(1024)
+        server_key = rsa.PublicKey.load_pkcs1(server_key)
 
-        chat(client)
+        threading.Thread(target=receive_messages, args=(client, username, private_key)).start()
+
+        chat(client, server_key)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,19 +1,31 @@
 import socket
 import threading
+import rsa
 
 HOST = '127.0.0.1'
 PORT = 1234
 LISTENERS_LIMIT = 4
 current_users = []
 
+server_public_k, server_private_k = rsa.newkeys(1024)
 
-def send_message(client, message):
-    client.sendall(message.encode())
+
+def send_key(client):
+    client.sendall(server_public_k.save_pkcs1("PEM"))
+
+
+def send_message(client, message, key):
+    message = rsa.encrypt(message.encode(), key)
+    client.sendall(message)
 
 
 def listen_for_messages(client, username):
     while True:
-        message = client.recv(2048).decode('utf-8')
+        message = client.recv(2048)
+        print(message)
+        message = rsa.decrypt(message, server_private_k)
+        print(message)
+        message = message.decode()
         if message:
             message = username + '~' + message
             send_messages_to_all(message)
@@ -23,14 +35,17 @@ def listen_for_messages(client, username):
 
 def send_messages_to_all(message):
     for user in current_users:
-        send_message(user[1], message)
+        send_message(user[1], message, user[2])
 
 
 def client_handle(client):
     while True:
         username = client.recv(2048).decode('utf-8')
         if username:
-            current_users.append((username, client))
+            pk = client.recv(1024)
+            pk = rsa.PublicKey.load_pkcs1(pk)
+            current_users.append((username, client, pk))
+            send_key(client)
             break
         else:
             print("Client username is Empty!")
